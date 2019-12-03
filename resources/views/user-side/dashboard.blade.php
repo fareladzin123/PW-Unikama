dec21ebd47962547389d785ff7b2071cc7ee88e4
unknown <julian18.ech@gmail.com>
2019-12-03 13:36:31 +0700

    aman ta

--- resources/views/user-side/dashboard.blade.php
+++ resources/views/user-side/dashboard.blade.php
@@ -8,6 +8,12 @@
     </ol>
   </div>
 
+  @if(Session('errors'))
+            <div class="alert alert-danger">
+                <div>Data Tidak Ditemukan</div>
+            </div>
+        @endif
+
   <label for="">Kategori Data</label>
   <div class="folder">
     @foreach($kategori as $k)
@@ -21,18 +27,18 @@
   @if(!empty($data))
   <label for="" class="mt-5">Terakhir Ditambahkan</label>
   <div class="folder">
-    @foreach($data as $d)
+    @foreach($data as $x =>$d)
       <div class="file">
         <div class="file-img" style="background: url(../uploads/images/{{$d->filename}});"></div>
         <div class="file-detail">
-          <a style="cursor: pointer" data-toggle="modal" data-target="#addSubCategory"><i class="fa fa-folder-open"></i>
+          <a style="cursor: pointer" data-toggle="modal" data-target="{{'#addSubCategory'.$x}}"><i class="fa fa-folder-open"></i>
             {{$d->nama_data}} <br></a>
             @if($d->created_at != null)
           <span class="d-block mt-4 text-right"><?php echo date_format($d->created_at,"N M Y - H.i A");?></span>
             @endif
         </div>
       </div>
-    <div class="modal fade" id="addSubCategory" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    <div class="modal fade" id="{{'addSubCategory'.$x}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
       aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
